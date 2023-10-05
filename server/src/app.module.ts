e990a05513d7ae84ee33ed68504f482bb4f998f5
Ayush Chugh <ayushchugh2006@gmail.com>
2023-10-05 09:27:43 +0530

    chore: eslint, prettier, husky
    closes #6,#5

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,17 @@
-import { MiddlewareConsumer, Module, UseFilters } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './modules/auth/auth.module';
-import { MailerModule } from './util/mailer/mailer.module';
-import { AllExceptionsFilter } from './filters/allExceptions.filter';
-import RequestLoggerMiddleware from './middleware/requestLogger.middleware';
+import { MiddlewareConsumer, Module, UseFilters } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AuthModule } from "./modules/auth/auth.module";
+import { MailerModule } from "./util/mailer/mailer.module";
+import { AllExceptionsFilter } from "./filters/allExceptions.filter";
+import RequestLoggerMiddleware from "./middleware/requestLogger.middleware";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, MailerModule],
-  controllers: [],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, MailerModule],
+	controllers: [],
 })
 @UseFilters(AllExceptionsFilter)
 export class AppModule {
-  configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
-  }
+	configure(consumer: MiddlewareConsumer): void {
+		consumer.apply(RequestLoggerMiddleware).forRoutes("*");
+	}
 }
--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,38 +1,27 @@
+import { Body, Controller, Get, HttpCode, Param, Post, Query } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
-import { AuthService } from './auth.service';
-import {
-  GetVerifyEmailParamsDTO,
-  GetVerifyEmailQueryDTO,
-  PostSignupBodyDTO,
-  PostSignupQueryDTO,
-} from './auth.dto';
+	GetVerifyEmailParamsDTO,
+	GetVerifyEmailQueryDTO,
+	PostSignupBodyDTO,
+	PostSignupQueryDTO,
+} from "./auth.dto";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
 
-  @Post('/signup')
-  @HttpCode(201)
-  async postSignup(
-    @Body() signupDto: PostSignupBodyDTO,
-    @Query() query: PostSignupQueryDTO,
-  ) {
-    return await this.authService.postSignup(signupDto, query);
-  }
+	@Post("/signup")
+	@HttpCode(201)
+	async postSignup(@Body() signupDto: PostSignupBodyDTO, @Query() query: PostSignupQueryDTO) {
+		return await this.authService.postSignup(signupDto, query);
+	}
 
-  @Get('/signup/verify/:id')
-  async getVerifyEmail(
-    @Param() params: GetVerifyEmailParamsDTO,
-    @Query() query: GetVerifyEmailQueryDTO,
-  ) {
-    return await this.authService.getVerifyEmail(params, query);
-  }
+	@Get("/signup/verify/:id")
+	async getVerifyEmail(
+		@Param() params: GetVerifyEmailParamsDTO,
+		@Query() query: GetVerifyEmailQueryDTO,
+	) {
+		return await this.authService.getVerifyEmail(params, query);
+	}
 }
